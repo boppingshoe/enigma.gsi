72d4bd03daca3623265bd7fa71d166fdf0d2c777
bhsu <bobby.hsu@alaska.gov>
2024-01-31 10:35:47 -0900

    add prep ichthy data

--- R/enigma_data.R
+++ R/enigma_data.R
@@ -185,6 +185,150 @@ prep_enigma_data <-
   }
 
 
+#' Prepping ichthy-genetics BMM input data
+#'
+#' @description
+#' Prep input data for Bayesian mixture model (i.e., Pella-Madusa model) integrated with ithchyphonus information.
+#'
+#' @param mixture_data Mixture data in GCL or \pkg{rubias} format.
+#' @param baseline_data Baseline data in GCL or \pkg{rubias} format.
+#' @param pop_info Population information for the baseline. A tibble with columns
+#'   `collection` (collection names), `repunit` (reporting unit names),
+#'   `grpvec` (group numbers), `origin` (wild/hatchery; optional).
+#' @param ichthy_status Ichthyphonus information for mixture individuals. It is a tibble with two columns:
+#'  * `indiv` - individual identification, the same as mixture data,
+#'  * `ich` - ichthyphonus status, 1 = positive, 0 = negative.
+#' @param file Where you want to save a copy of input data as a RDS file.
+#'   Need to type out full path and extension `.Rds`.
+#'   Leave it empty if you don't want to save a copy.
+#' @param loci Optional. Provide loci for the mixture or baseline as a fail-safe check.
+#'
+#' @return A list objects as the input data for enigma_mdl()
+#'
+#' @importFrom magrittr %>%
+#'
+#' @examples
+#' # prep input data
+#' ichthy_data <- prep_ichthy_data(mixture_data = mix, baseline_data = baseline, pop_info = ayk_pops60, ichthy_status = ichthy)
+#'
+#' @export
+prep_ichthy_data <-
+  function(mixture_data, baseline_data, pop_info, ichthy_status, file = NULL, loci = NULL) {
+
+    start_time <- Sys.time()
+
+    # identify loci for each stage
+    # make sure no colnames other than marker names have ".1" at the end
+    loci_base <-
+      dplyr::tibble(locus = names(baseline_data)) %>%
+      dplyr::filter(grepl("\\.1$", locus)) %>%
+      dplyr::mutate(locus = substr(locus, 1, nchar(locus) - 2)) %>%
+      dplyr::pull(locus)
+
+    # input error check
+
+    loci_mix <-
+      dplyr::tibble(locus = names(mixture_data)) %>%
+      dplyr::filter(grepl("\\.1$", locus)) %>%
+      dplyr::mutate(locus = substr(locus, 1, nchar(locus) - 2)) %>%
+      dplyr::pull(locus)
+
+    error_message <- check_loci_pops(loci, loci_base, loci_mix)
+
+    if ("all good" %in% error_message) {
+      loci <- loci_base
+      message("Compiling input data, may take a minute or two...")
+    } else {
+      stop(error_message)
+    }
+
+    # change column name if the data are gcl objects
+    # to match rubias input data name convention
+    if ("SILLY_CODE" %in% names(baseline_data))
+      baseline_data <- dplyr::rename(baseline_data, collection = SILLY_CODE)
+
+    if ("SillySource" %in% names(mixture_data))
+      mixture_data <- dplyr::rename(mixture_data, indiv = SillySource)
+
+    # tally allele for each baseline and mixture sample
+    base <- allefreq(baseline_data, baseline_data, loci, collect_by = collection) %>%
+      dplyr::right_join(pop_info, by = c("collection" = "collection"), keep = FALSE) %>%
+      dplyr::relocate(!dplyr::ends_with(as.character(0:9)), .after = collection) %>%
+      dplyr::mutate(dplyr::across(dplyr::ends_with(as.character(0:9)), ~tidyr::replace_na(., 0)))
+
+    mix <- allefreq(mixture_data, baseline_data, loci)
+
+    # numbers of allele types
+    nalleles <- lapply(loci, function(loc) {
+      dplyr::tibble(locus = loc,
+                    call = baseline_data %>%
+                      dplyr::select(dplyr::all_of(loc), paste0(loc, ".1")) %>%
+                      unlist() %>% unique() %>% .[!is.na(.)],
+                    altyp = seq.int(dplyr::n_distinct(call)) %>% factor())
+    }) %>% dplyr::bind_rows() %>%
+      dplyr::group_by(locus) %>%
+      dplyr::summarise(n_allele = max(as.numeric(altyp)), .groups = "drop")
+
+    n_alleles <- nalleles %>%
+      dplyr::pull(n_allele) %>%
+      stats::setNames(nalleles$locus)
+
+    # group names
+    grp_nms <- base %>%
+      dplyr::arrange(grpvec) %>%
+      dplyr::pull(repunit) %>%
+      unique()
+
+    # wild or hatchery
+    if ("origin" %in% names(base)) {
+      wildpops <- base %>%
+        dplyr::filter(origin == "wild") %>%
+        dplyr::pull(collection)
+      hatcheries <- base %>%
+        dplyr::filter(origin == "hatchery") %>%
+        dplyr::pull(collection)
+    } else {
+      wildpops <- base %>% dplyr::pull(collection)
+      hatcheries <- NULL
+    }
+
+    # iden if specified in mixture data
+    if (any(grepl("known_", names(mixture_data)))) {
+      iden <- mixture_data %>%
+        dplyr::select(tidyr::contains("known_")) %>%
+        dplyr::pull()
+      if (!all(stats::na.omit(iden) %in% c(wildpops, hatcheries))) {
+        stop(c("Unidentified populations found in 'known_collection': ",
+               paste0(unique(stats::na.omit(iden)[which(!stats::na.omit(iden) %in% c(wildpops, hatcheries))]), ", ")))
+      }
+      iden <- factor(iden, levels = c(wildpops, hatcheries)) %>%
+        as.numeric()
+    } else {
+      iden <- NULL
+    }
+
+    # output
+    ich_dat = list(
+      x = mix,
+      y = base,
+      iden = iden,
+      nalleles = n_alleles,
+      groups = base$grpvec,
+      group_names = grp_nms,
+      wildpops = wildpops,
+      hatcheries = hatcheries,
+      ichthy_status = ichthy_status
+    )
+
+    if (!is.null(file)) saveRDS(ich_dat, file = file)
+
+    print(Sys.time() - start_time)
+
+    return(ich_dat)
+
+  }
+
+
 #' Allele frequency
 #'
 #' Calculate allele frequency for each locus
